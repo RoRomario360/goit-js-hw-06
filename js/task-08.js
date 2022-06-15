abd139d409006712157fb0b86a4af0b6d8e39c74
RoRomario360 <fotoromario@gmail.com>
2022-06-15 18:22:15 +0300

    task-10 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", onLoginForm);
+
+function onLoginForm(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Все поля должны быть заполнены!");
+  }
+
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const body = document.querySelector("body");
+const changeColor = document.querySelector(".change-color");
+
+changeColor.addEventListener("click", getRandomHexColor);
+
+function getRandomHexColor(event) {
+  return (body.style.backgroundColor = `#${Math.floor(
+    Math.random() * 16777215
+  ).toString(16)}`);
 }
